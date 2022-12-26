1375696e5be7d068696fdd32423cc6c216b73c83
Albert481 <albertyu481@gmail.com>
2022-12-27 00:54:19 +0800

    added screenshots for readme

--- openjio-front/src/features/auth/authService.js
+++ openjio-front/src/features/auth/authService.js
@@ -17,28 +17,6 @@ const googleSignIn = async (credentials) => {
     return response.data   
 }
 
-// Custom
-// Register user
-const register = async (userData) => {
-    const response = await axios.post(API_URL, userData)
-    
-    if (response.data) {
-        localStorage.setItem('user', JSON.stringify(response.data))
-    }
-    return response.data
-}
-
-// Login user
-const login = async (userData) => {
-    const response = await axios.post(API_URL + 'login', userData)
-
-    if (response.data) {
-        localStorage.setItem('user', JSON.stringify(response.data))
-    }
-
-    return response.data
-}
-
 // Logout user
 const logout = () => {
     localStorage.removeItem('user')
@@ -46,9 +24,7 @@ const logout = () => {
 
 const authService = {
     googleSignIn,
-    register,
-    logout,
-    login,
+    logout
 }
 
 export default authService;
